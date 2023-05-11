f92feee40cbf567bb231a2120da9dcdab6718aff
Gene Suhir <gsuhir@gmail.com>
2023-05-11 09:24:05 -0700

    added Assets folder with screenshot, removed previous mockup image, cleaned up CSS

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-var apiKey = "271eb3166d39afc7090c6ceb0e032766";
+// Initialize apiKey and baseURL
 
-// Initialize variables
+var apiKey = "271eb3166d39afc7090c6ceb0e032766";
+var baseURL = "https://api.openweathermap.org/data/2.5/forecast?lat=(lat)&lon=(lon)&appid=271eb3166d39afc7090c6ceb0e032766"
+
+// Initialize variables from the HTML
 var currentWeatherEl = document.getElementById("current-weather");
 var forecastEl = document.getElementById("forecast");
 var historyListEl = document.getElementById("history-list");
@@ -9,13 +12,13 @@ var searchHistory = [];
 // Event listener for form submission
 document.querySelector("form").addEventListener("submit", function(event) {
   event.preventDefault();
-  let city = document.getElementById("city-input").value;
+  var city = document.getElementById("city-input").value;
   getWeatherData(city);
 });
 
 // Event listener for search history item click
 historyListEl.addEventListener("click", function(event) {
-  let city = event.target.textContent;
+  var city = event.target.textContent;
   getWeatherData(city);
 });
 
@@ -30,39 +33,41 @@ function getWeatherData(city) {
 
   // API request for current weather
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`)
-    .then(response => response.json())
-    .then(data => {
-      // Display current weather data
-      let cityName = data.name;
-      let date = new Date(data.dt * 1000).toLocaleDateString();
-      let weatherIcon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
-      let temp = Math.round(data.main.temp);
-      let humidity = data.main.humidity;
-      let windSpeed = Math.round(data.wind.speed);
-      currentWeatherEl.innerHTML = `
-        <h2>${cityName} (${date}) <img src="${weatherIcon}" alt="${data.weather[0].description}"></h2>
-        <p>Temperature: ${temp} &deg;F</p>
-        <p>Wind Speed: ${windSpeed} mph</p>
-        <p>Humidity: ${humidity}%</p>
-        <br>
-        <h3>5-Day Forecast:</h3>
-        
-      `;
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    var cityName = data.name;
+    var date = new Date(data.dt * 1000).toLocaleDateString();
+    var weatherIcon = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    var temp = Math.round(data.main.temp);
+    var humidity = data.main.humidity;
+    var windSpeed = Math.round(data.wind.speed);
+    currentWeatherEl.innerHTML = `
+      <h2>${cityName} (${date}) <img src="${weatherIcon}" alt="${data.weather[0].description}"></h2>
+      <p>Temperature: ${temp} &deg;F</p>
+      <p>Wind Speed: ${windSpeed} mph</p>
+      <p>Humidity: ${humidity}%</p>
+      <br>
+      <h3>5-Day Forecast:</h3>
+    `;
 
       // API request for 5-day forecast
       return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`);
     })
-    .then(response => response.json())
-    .then(data => {
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
       // Display 5-day forecast data
       let forecastHtml = "";
       for (let i = 0; i < data.list.length; i += 8) {
-        let forecast = data.list[i];
-        let date = new Date(forecast.dt * 1000).toLocaleDateString();
-        let weatherIcon = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
-        let temp = Math.round(forecast.main.temp);
-        let humidity = forecast.main.humidity;
-        let windSpeed = Math.round(forecast.wind.speed);
+        var forecast = data.list[i];
+        var date = new Date(forecast.dt * 1000).toLocaleDateString();
+        var weatherIcon = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+        var temp = Math.round(forecast.main.temp);
+        var humidity = forecast.main.humidity;
+        var windSpeed = Math.round(forecast.wind.speed);
         forecastHtml += `
           <div class="forecast-item">
             <h3>${date}</h3>
@@ -77,9 +82,10 @@ function getWeatherData(city) {
       
       forecastEl.innerHTML = forecastHtml;
     })
+    
     .catch(error => {
       console.log(error);
-      currentWeatherEl.innerHTML = "<p>Please enter a city name.</p>";
+      currentWeatherEl.innerHTML = "<p>Please enter a city name!</p>";
       forecastEl.innerHTML = "";
     });
 }
@@ -97,6 +103,6 @@ function updateHistoryList() {
     }
   }
   
-  // Initialize search history list on page load
+  // Initialize search history list on page refresh
   updateHistoryList();
   
